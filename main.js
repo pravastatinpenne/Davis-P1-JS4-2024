0d0a2ffc39d551640169f6b47a98b28d1159a6f7
pravastatinpenne <160611833+pravastatinpenne@users.noreply.github.com>
2024-02-21 08:03:22 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+const lightbulb = document.getElementById('lightbulb');
+const lightswitch = document.getElementById('lightswitch');
+const status = document.getElementById('statusOfSwitch');
+
+let isLightOn = false;
+
+lightswitch.addEventListener('click', function() {
+    if(isLightOn) {
+        lightbulb.src = "lightOff.png";
+        lightswitch.src = "switchOff.png";
+        status.textContent = "TURN THE LIGHT ON";
+    }else{
+        lightbulb.src = "lightOn.png";
+        lightswitch.src = "switchON.png";
+        status.textContent = "You DID IT!, Now Turn off the Lights."
+    }
+    isLightOn = !isLightOn;
+}
+
+);
